4b766bfc7a61d2602a6d190628cdd687c729a678
Rodrigue Ntagashobotse <112996908+RNtag12@users.noreply.github.com>
2024-06-19 02:18:32 +1000

    Update frequencyAnalysis.py

--- frequencyAnalysis.py
+++ frequencyAnalysis.py
@@ -1,1 +1,7 @@
-def 
+#letters frequency in the alphabet are specified here
+ETAOIN='ETAOINSHRDLCUMWFGYPBVKJXQZ'
+
+#letters of the alphabet are specified here
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def getLetterCount(message):
