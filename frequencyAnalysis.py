5e3873326a2b21ef7687871120116b5dd8887b00
Rodrigue Ntagashobotse <112996908+RNtag12@users.noreply.github.com>
2024-06-22 14:06:03 +1000

    frequencyAnalysis.py

--- frequencyAnalysis.py
+++ frequencyAnalysis.py
@@ -4,8 +4,8 @@ ETAOIN='ETAOINSHRDLCUMWFGYPBVKJXQZ'
 #letters of the alphabet are specified here
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-# the first step is to create a dictionary with keys of single letters and we ensure that the values of the
-    # count of how many times they appear in the message parameter:
+# the first step is to create a dictionary with keys of single letters and ensure that the values of the
+# count of how many times they appear in the message parameter:
 def getLetterCount(message):
   letterCount = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
 
@@ -14,3 +14,7 @@ for Letter in message.upper():
     letterCount[letter] += 1
     return letterCount
   
+def getItemAtIndexZero(items):
+    return items[0]
+def 
+    
